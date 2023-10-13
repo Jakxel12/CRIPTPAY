1337671633ee2ddf437ffb237e4cb32459fac459
Jakxel12 <147444918+Jakxel12@users.noreply.github.com>
2023-10-12 19:31:12 -0700

    Update app.js
    CODIGO BASE DE JS

---
+++ app.js
@@ -0,0 +1,7 @@
+document.querySelector('#inicio').addEventListener('click', () => {
+    alert('Has hecho clic en la sección de inicio.');
+});
+
+document.querySelector('#acerca-de').addEventListener('click', () => {
+    alert('Has hecho clic en la sección Acerca de Mí.');
+});
